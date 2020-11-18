bf282b0d9a6a7d06bc2c079bace0cf7bd53b263f
hockeyqqy <qqu@slb.com>
2020-11-18 15:02:50 +0800

    feat: return 2 given 2

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string Say(int count)
+        {
+            if (count == 3)
+            {
+                return "Fizz";
+            }
+
+            return count.ToString();
+        }
+    }
+}
---
+++ FizzBuzzTest/FizzBuzzTest.cs
@@ -0,0 +1,69 @@
+using FizzBuzz;
+using Moq;
+using Xunit;
+
+namespace FizzBuzzTest
+{
+    public class FizzBuzzTest
+    {
+        [Fact]
+        public void FizzBuzz_Should_Return_1_Given_1()
+        {
+            //given
+            //Dependency dependency = new Dependency();
+            //HelloWorld helloWorld = new HelloWorld(dependency);
+            FizzBuzz.FizzBuzz fizzBuzz = new FizzBuzz.FizzBuzz();
+            //when
+            string actual = fizzBuzz.Say(1);
+            string expect = "1";
+            //then
+            //Assert.Equal("Leave me alone.", actual);
+            Assert.Equal(expect, actual);
+        }
+
+        [Fact]
+        public void FizzBuzz_Should_Return_2_Given_2()
+        {
+            //given
+            //Dependency dependency = new Dependency();
+            //HelloWorld helloWorld = new HelloWorld(dependency);
+            FizzBuzz.FizzBuzz fizzBuzz = new FizzBuzz.FizzBuzz();
+            //when
+            string actual = fizzBuzz.Say(2);
+            string expect = "2";
+            //then
+            //Assert.Equal("Leave me alone.", actual);
+            Assert.Equal(expect, actual);
+        }
+
+        [Fact]
+        public void FizzBuzz_Should_Return_Fizz_Given_3()
+        {
+            //given
+            //Dependency dependency = new Dependency();
+            //HelloWorld helloWorld = new HelloWorld(dependency);
+            FizzBuzz.FizzBuzz fizzBuzz = new FizzBuzz.FizzBuzz();
+            //when
+            string actual = fizzBuzz.Say(3);
+            string expect = "Fizz";
+            //then
+            //Assert.Equal("Leave me alone.", actual);
+            Assert.Equal(expect, actual);
+        }
+
+        //[Fact]
+        //public void Should_be_mocked()
+        //{
+        //    //given
+        //    var mockDependencu = new Mock<Dependency>();
+        //    mockDependencu.Setup(m => m.Say()).Returns("Hello World");
+        //    HelloWorld helloWorld = new HelloWorld(mockDependencu.Object);
+
+        //    //when
+        //    string actual = helloWorld.BeenCalled();
+
+        //    //then
+        //    Assert.Equal("Hello World", actual);
+        //}
+    }
+}
